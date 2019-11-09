39f9a52a19d29019bcd322fb350d9a75e80afe23
s-caarl <cantado@nevada.unr.edu>
2019-11-08 16:47:23 -0800

    Created dummy functions addition, subtraction, main

---
+++ calculator.c
@@ -0,0 +1,24 @@
+//integer calculator
+
+#include <stdio.h>
+
+int addition(int x, int y);
+int subtraction(int x, int y);
+
+int main()
+{
+	
+	return 0;
+}
+
+int addition(int x, int y)
+{
+	
+	return 0;
+}
+
+int subtraction(int x, int y)
+{
+	
+	return 0;
+}
