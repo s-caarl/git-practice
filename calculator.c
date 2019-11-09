e4af64c325807ca6d3f13a0140d0091dff549bfa
s-caarl <cantado@nevada.unr.edu>
2019-11-08 17:00:39 -0800

    Added display menu for addition and subtraction

--- calculator.c
+++ calculator.c
@@ -7,6 +7,38 @@ int subtraction(int x, int y);
 
 int main()
 {
+	int a, b, menu;
+	printf("***Integer Calculator***\n");
+	do
+	{
+		printf("1. Addition\n2. Subtraction\n0. EXIT\n");
+		scanf("%d", &menu);
+		
+		switch (menu)
+		{
+			case 1:
+				printf("Enter value 1: ");
+				scanf("%d", &a);
+				printf("Enter value 2: ");
+				scanf("%d", &b);
+				printf("The sum of %d and %d is %d\n", a, b, addition(a, b));
+				break;
+			case 2:
+				printf("Enter value 1: ");
+				scanf("%d", &a);
+				printf("Enter value 2: ");
+				scanf("%d", &b);
+				printf("The difference of %d and %d is %d\n", a, b, subtraction(a, b));
+				break;
+			case 0:
+				menu = 0;
+				break;
+			default:
+				printf("Invalid option. Please try again.\n");
+				break;
+		}
+	}
+	while (menu != 0);
 	
 	return 0;
 }
